92e94e2a651812e182febc305064b889b61f37f6
OniMatt <mthssimioni@gmail.com>
2023-08-27 18:47:33 -0300

    feat(resource/ClienteResource): implementa endpoints cliente

--- src/main/java/com/hamburgueria/resource/ClienteResource.java
+++ src/main/java/com/hamburgueria/resource/ClienteResource.java
@@ -3,15 +3,11 @@ package com.hamburgueria.resource;
 import com.hamburgueria.domain.Cliente;
 import com.hamburgueria.repository.ClienteRepository;
 
+import io.quarkus.logging.Log;
 import jakarta.inject.Inject;
 import jakarta.json.bind.Jsonb;
 import jakarta.json.bind.JsonbBuilder;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
@@ -25,18 +21,20 @@ public class ClienteResource {
 
     @GET
     @Produces( MediaType.APPLICATION_JSON )
-    public String listClienteByEmail( @QueryParam( "email" ) String email ) {
-        return jsonb.toJson( clienteRepository.getClienteByEmail( email ) );
+    public String listClienteByCpfOrEmail( @QueryParam( "email" ) String email, @QueryParam( "cpf" ) String cpf ) {
+        return jsonb.toJson( clienteRepository.getClienteByCpfOrEmail( cpf, email ) );
     }
 
     @POST
     @Consumes( MediaType.APPLICATION_JSON )
-    public Response createCliente( String json ) {
+    public Response saveCliente( String json ) {
         try {
             clienteRepository.saveCliente( jsonb.fromJson( json, Cliente.class ) );
             return Response.ok().build();
         } catch ( Exception e ) {
+            Log.info(e.getMessage());
             return Response.serverError().build();
         }
     }
+
 }
