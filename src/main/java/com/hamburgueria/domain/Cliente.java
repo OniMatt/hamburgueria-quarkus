6ea6170c04b94e106dd1d576daf53afe88996d36
OniMatt <mthssimioni@gmail.com>
2023-08-27 18:41:49 -0300

    feat(domain/Cliente): adiciona cpf

--- src/main/java/com/hamburgueria/domain/Cliente.java
+++ src/main/java/com/hamburgueria/domain/Cliente.java
@@ -9,12 +9,14 @@ public class Cliente extends PanacheEntity {
   private String senha;
   private String telefone;
   private String email;
+  private String cpf;
 
-  public Cliente( String nome, String senha, String telefone, String email ) {
+  public Cliente( String nome, String senha, String telefone, String email, String cpf ) {
     this.nome = nome;
     this.senha = senha;
     this.telefone = telefone;
     this.email = email;
+    this.cpf = cpf;
   }
 
   public Cliente() {
@@ -50,6 +52,14 @@ public class Cliente extends PanacheEntity {
 
   public void setEmail( String email ) {
     this.email = email;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
   }
 
 }
